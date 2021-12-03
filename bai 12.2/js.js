2f1739bd53613fc4b5d612546f95aa5f94acc4dd
tabao1995 <91716368+tabao1995@users.noreply.github.com>
2021-12-03 21:16:47 +0700

    sửa
    sửa

--- bai 12.2/js.js
+++ bai 12.2/js.js
@@ -1,8 +1,12 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fpass"].value;
-    let y = document.forms["myForm"]["rePassword"].value;
-    if (x !== y ) {
+  function validateForm() {
+      let k = document.forms["myForm"]["fname"].value;
+      let x = document.forms["myForm"]["fpass"].value;
+      let y = document.forms["myForm"]["rePassword"].value;
+      if (k == '' && y == '' && x == '')
+        { alert ("Please Fill ALL");
+        return false;
+      }
+      if(x !== y ) {
       alert("Re-password is incorrect");
-      return false;
+      return false;}
     }
-  }
